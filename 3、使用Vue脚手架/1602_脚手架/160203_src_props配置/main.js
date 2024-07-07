008b7d1d488dc27a95cb3789c8119ba3b48ab8b0
ryualvin <lsben1014@gmail.com>
2024-07-07 10:44:18 +0900

    backup

--- 3、使用Vue脚手架/1602_脚手架/160203_src_props配置/main.js
+++
@@ -1,7 +0,0 @@
-import App from "./App.vue";
-import Vue from "vue";
-Vue.config.procutionTip = false;
-new Vue({
-  el: "#app",
-  render: (h) => h(App),
-});
--- 3、使用Vue脚手架/1602_脚手架/vue.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require("@vue/cli-service");
-module.exports = defineConfig({
-  transpileDependencies: true,
-  lintOnSave: false,
-  // 开启代理服务器，这种方式不能开启多台代理
-  // devServer: {
-  //   proxy: 'http://localhost:5000'
-  // }
-});
