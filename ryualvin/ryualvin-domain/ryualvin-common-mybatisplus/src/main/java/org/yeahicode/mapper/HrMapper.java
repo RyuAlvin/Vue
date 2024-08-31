ac21179b2909a4159299786667f9fe089965a071
ryualvin <lsben1014@gmail.com>
2024-08-31 23:00:50 +0900

    整合Mybatis-Plus（@Mapper扫描方式）

--- ryualvin/ryualvin-domain/ryualvin-common-mybatisplus/src/main/java/org/yeahicode/mapper/HrMapper.java
+++ ryualvin/ryualvin-domain/ryualvin-common-mybatisplus/src/main/java/org/yeahicode/mapper/HrMapper.java
@@ -1,7 +1,9 @@
 package org.yeahicode.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
 import org.yeahicode.model.Hr;
 
+@Mapper
 public interface HrMapper extends BaseMapper<Hr> {
 }
--- ryualvin/ryualvin-domain/ryualvin-common-mybatisplus/src/main/java/org/yeahicode/repository/MybatisPlusConfiguration.java
+++ ryualvin/ryualvin-domain/ryualvin-common-mybatisplus/src/main/java/org/yeahicode/repository/MybatisPlusConfiguration.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@MapperScan({"org.yeahicode.mapper"})
+//@MapperScan({"org.yeahicode.mapper"})
 public class MybatisPlusConfiguration {
     /**
      * mybatisplus分页插件
