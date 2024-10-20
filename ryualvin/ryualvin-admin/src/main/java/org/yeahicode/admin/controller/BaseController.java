414135fd9bcd5b084bc11254767cfbbb3648fa3e
ryualvin <lsben1014@gmail.com>
2024-10-20 17:05:13 +0900

    统一路由BaseController

---
+++ ryualvin/ryualvin-admin/src/main/java/org/yeahicode/admin/controller/BaseController.java
@@ -0,0 +1,10 @@
+package org.yeahicode.admin.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin/v1")
+public class BaseController {
+    // 可存放一些共通注入
+}
--- ryualvin/ryualvin-admin/src/main/java/org/yeahicode/admin/controller/HrController.java
+++ ryualvin/ryualvin-admin/src/main/java/org/yeahicode/admin/controller/HrController.java
@@ -1,14 +1,17 @@
 package org.yeahicode.admin.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.yeahicode.application.dto.HrDto;
 import org.yeahicode.application.service.HrService;
 
-@RestController
-public class HrController {
+@Controller // 即使继承父类controller，子类controller也必须要标明
+//@RequestMapping("/admin/v2") // 如果在继承父类路由的情况下，子类重新定义路由，则会覆盖父类路由（同继承思想一样，子类有的话就用子类的）
+public class HrController extends BaseController {
 
     @Autowired
     HrService hrService;
