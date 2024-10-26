71ac5bc43512b2282cf85718bf17d670cb7bd0cb
ryualvin <lsben1014@gmail.com>
2024-10-26 17:30:02 +0900

    Controller基类追加

--- ryualvin/ryualvin-admin/src/main/java/org/yeahicode/admin/controller/HrController.java
+++ ryualvin/ryualvin-admin/src/main/java/org/yeahicode/admin/controller/HrController.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.yeahicode.admin.controller.commons.BaseController;
 import org.yeahicode.application.dto.HrDto;
 import org.yeahicode.application.service.HrService;
 
+/**
+ * 测试Controller
+ */
 @Controller // 即使继承父类controller，子类controller也必须要标明
 //@RequestMapping("/admin/v2") // 如果在继承父类路由的情况下，子类重新定义路由，则会覆盖父类路由（同继承思想一样，子类有的话就用子类的）
 public class HrController extends BaseController {
--- ryualvin/ryualvin-admin/src/main/java/org/yeahicode/admin/controller/commons/BaseController.java
+++ ryualvin/ryualvin-admin/src/main/java/org/yeahicode/admin/controller/commons/BaseController.java
@@ -1,4 +1,4 @@
-package org.yeahicode.admin.controller;
+package org.yeahicode.admin.controller.commons;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
