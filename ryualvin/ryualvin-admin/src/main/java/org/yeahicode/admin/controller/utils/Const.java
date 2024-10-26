fb24ded2fb5a2e243707f1c384706acf8f77431d
ryualvin <lsben1014@gmail.com>
2024-10-26 19:57:30 +0900

    常数类用接口接口+变量代替

--- ryualvin/ryualvin-admin/src/main/java/org/yeahicode/admin/controller/utils/Const.java
+++ ryualvin/ryualvin-admin/src/main/java/org/yeahicode/admin/controller/utils/Const.java
@@ -1,9 +1,10 @@
 package org.yeahicode.admin.controller.utils;
 
-public class Const {
-    public static final Integer SUCCESS_CODE = 20000;
-    public static final String SUCCESS_MSG = "访问成功";
+// 也可将常量类定义成接口，接口中的变量默认都是public static final修饰，即全局常量
+public interface Const {
+    Integer SUCCESS_CODE = 20000;
+    String SUCCESS_MSG = "访问成功";
 
-    public static final Integer NO_ADMIN_CODE = 40000;
-    public static final String NO_ADMIN_MSG = "NO ADMIN !!!";
+    Integer NO_ADMIN_CODE = 40000;
+    String NO_ADMIN_MSG = "NO ADMIN !!!";
 }
