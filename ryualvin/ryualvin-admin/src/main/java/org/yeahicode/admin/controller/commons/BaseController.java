9c7d6f82475b2d6c480b577e9d2d903d832b5223
ryualvin <lsben1014@gmail.com>
2024-10-26 19:26:23 +0900

    ResponseEntity使用示例

--- ryualvin/ryualvin-admin/src/main/java/org/yeahicode/admin/controller/commons/BaseController.java
+++ ryualvin/ryualvin-admin/src/main/java/org/yeahicode/admin/controller/commons/BaseController.java
@@ -1,10 +1,11 @@
 package org.yeahicode.admin.controller.commons;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @RequestMapping("/admin/v1")
+@Controller
 public class BaseController {
     // 可存放一些共通注入
 }
