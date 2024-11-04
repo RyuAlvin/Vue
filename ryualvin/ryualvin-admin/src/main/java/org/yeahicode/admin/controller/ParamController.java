7c7f896acc6df19b45d1483e638c267ca63ea3fd
ryualvin <lsben1014@gmail.com>
2024-11-04 22:27:09 +0900

    参数注入处理@Value()

--- ryualvin/ryualvin-admin/src/main/java/org/yeahicode/admin/controller/ParamController.java
+++ ryualvin/ryualvin-admin/src/main/java/org/yeahicode/admin/controller/ParamController.java
@@ -1,6 +1,7 @@
 package org.yeahicode.admin.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,15 +13,29 @@ import java.util.Map;
 public class ParamController {
 
     @Autowired
-    Environment env;
+    private Environment env;
 
-    @GetMapping("/getParam")
-    public Map<String, Object> getParam() {
+    @Value("${ryualvin.address}")
+    private String addrField;
+
+    @Value("${JAVA_HOME}")
+    private String javaHomeField;
+
+    @GetMapping("/getParam1")
+    public Map<String, Object> getParam1() {
         String addr = env.getProperty("ryualvin.address");
         String javaHome = env.getProperty("JAVA_HOME");
         Map<String, Object> map = new HashMap<>();
         map.put("addr", addr);
         map.put("javaHome", javaHome);
+        return map;
+    }
+
+    @GetMapping("/getParam2")
+    public Map<String, Object> getParam2() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("addr", addrField);
+        map.put("javaHome", javaHomeField);
         return map;
     }
 }
