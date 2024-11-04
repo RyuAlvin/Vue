61c37d71c99e6635a8129fd0c6e25dc8fc3a7111
ryualvin <lsben1014@gmail.com>
2024-11-04 23:26:52 +0900

    拦截器配置

---
+++ ryualvin/ryualvin-admin/src/main/java/org/yeahicode/admin/controller/commons/interceptor/SecurityInterceptor.java
@@ -0,0 +1,32 @@
+package org.yeahicode.admin.controller.commons.interceptor;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 该拦截器作为组件注册到容器中
+ * 继承的接口HandlerInterceptor中，以下三个都属于默认方法，所以不是强制重写，而是可以选择性重写
+ */
+@Component
+public class SecurityInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        System.out.println("preHandle...");
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        System.out.println("postHandle...");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        System.out.println("afterCompletion...");
+    }
+}
