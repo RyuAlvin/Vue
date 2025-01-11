5d33ac045806f01a120381b8bc9bc1177bf9c328
ryualvin <lsben1014@gmail.com>
2025-01-11 22:12:15 +0900

    将template以及相关数据方法抽取成对象组件

--- Vue/13-webpack的使用/13-04-webpack配置vue/dist/bundle.js
+++ Vue/13-webpack的使用/13-04-webpack配置vue/dist/bundle.js
@@ -289,16 +289,32 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_vue__ = __webpack_require__(3);
 
 
-const app = new __WEBPACK_IMPORTED_MODULE_0_vue__["a" /* default */]({
-  el: '#app',
+// 提取App作为组件导入
+const App = {
+  // 用该template去替换<div id='app'></div>
   template: `
-<div>
-  <h1>我是APP</h1>
-  <p>{{message}}</p>
-</div>
-  `,
-  data: {
-    message: '我是APP的内容'
+    <div>
+      <h1>我是App</h1>
+      <p>{{message}}</p>
+      <button @click="btnClick">按钮1</button>
+    </div>
+      `,
+  data() {
+    return {
+      message: '我是APP的内容'
+    }
+  },
+  methods: {
+    btnClick() {
+      console.log('Hello App');
+    }
+  }
+}
+
+new __WEBPACK_IMPORTED_MODULE_0_vue__["a" /* default */]({
+  el: '#app',
+  components: {
+    App
   }
 })
 
--- Vue/13-webpack的使用/13-04-webpack配置vue/src/main.js
+++ Vue/13-webpack的使用/13-04-webpack配置vue/src/main.js
@@ -1,15 +1,30 @@
 import Vue from 'vue'
 
-const app = new Vue({
-  el: '#app',
+// 提取App作为组件导入
+const App = {
   // 用该template去替换<div id='app'></div>
   template: `
-<div>
-  <h1>我是APP</h1>
-  <p>{{message}}</p>
-</div>
-  `,
-  data: {
-    message: '我是APP的内容'
+    <div>
+      <h1>我是App</h1>
+      <p>{{message}}</p>
+      <button @click="btnClick">按钮1</button>
+    </div>
+      `,
+  data() {
+    return {
+      message: '我是APP的内容'
+    }
+  },
+  methods: {
+    btnClick() {
+      console.log('Hello App');
+    }
+  }
+}
+
+new Vue({
+  el: '#app',
+  components: {
+    App
   }
 })
