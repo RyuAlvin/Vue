cfd92ed36f7ef2d006faa52611b7bb5b44a91130
ryualvin <lsben1014@gmail.com>
2025-01-13 15:17:33 +0900

    合并base.config.js

--- Vue/13-webpack的使用/13-06-webpack配置分离/build/dev.config.js
+++ Vue/13-webpack的使用/13-06-webpack配置分离/build/dev.config.js
@@ -1,8 +1,11 @@
 /**
  * 只保留开发相关的本地服务器配置
  */
-
-module.exports = {
+// 安装 npm install --save-dev webpack-merge@4.1.5
+const WebpackMerge = require('webpack-merge')
+const baseConfig = require('./base.config')
+// 合并base.config.js
+module.exports = WebpackMerge(baseConfig, {
   devServer: {
     // 为哪一个文件夹提供本地服务，默认是根文件夹，我们这里要填写./dist
     contentBase: './dist',
@@ -13,4 +16,4 @@ module.exports = {
     // 在SPA页面中，依赖HTML5的history模式
     // historyApiFallback:
   }
-};
+})
--- Vue/13-webpack的使用/13-06-webpack配置分离/build/prod.config.js
+++ Vue/13-webpack的使用/13-06-webpack配置分离/build/prod.config.js
@@ -2,10 +2,13 @@
  * 只保留生产相关的js压缩配置
  */
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-
-module.exports = {
+// 安装 npm install --save-dev webpack-merge@4.1.5
+const WebpackMerge = require('webpack-merge')
+const baseConfig = require('./base.config')
+// 合并base.config.js
+module.exports = WebpackMerge(baseConfig, {
   plugins: [
     // 报错!!!!!!!!!!!!!!!!!!!!!!!!!!，跳过~
     new UglifyJsPlugin()
   ]
-};
+})
