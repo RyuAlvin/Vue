56b556cc82247edb62eacf3abf28f3091b278721
ryualvin <lsben1014@gmail.com>
2025-01-14 22:57:26 +0900

    关闭eslint检查

--- Vue/14-vuecli2test/src/main.js
+++ Vue/14-vuecli2test/src/main.js
@@ -11,3 +11,18 @@ new Vue({
   components: { App },
   template: '<App/>'
 })
+
+// eslint检查一下代码无法通过
+function sum(num1, num2) {
+  return num1 + num2;
+}
+
+console.log(sum(1, 3));
+
+// 按照以下修改，eslin检查就可以通过。
+// 不是所有的eslint标准都适用我们普通的代码规范。
+// function sum (num1, num2) {
+//   return num1 + num2
+// }
+
+// console.log(sum(1, 3))
