c8a3fdf307a3a60ed1bd61a49ca16d3fbe2b8f03
ryualvin <lsben1014@gmail.com>
2025-01-26 15:19:43 +0900

    组件独享守卫beforeRouteLeave+<keep-alive>组件的生命周期activated钩子函数缓存嵌套组件状态

--- Vue/17-learnvuerouter/src/components/Home.vue
+++ Vue/17-learnvuerouter/src/components/Home.vue
@@ -8,6 +8,11 @@
 
 <script>
 export default {
+  data() {
+    return {
+      path:'/home/news'
+    }
+  },
   /**
    * 通过<keep-alive>包裹的组件在切换时不会被销毁，而是保留其状态，提高性能。
    * 可通过以下created和destroyed两个钩子函数观察证明。
@@ -19,6 +24,27 @@ export default {
   destroyed() {
     console.log('Home cpn is destroyed...');
   },
+  // 在离开前记住当前路径
+  beforeRouteLeave (to, from, next) {
+    this.path = this.$route.path;
+    console.log('Home path ---> ', this.path);
+    next();
+  },
+  /**
+   * activated 和 deactivated 是 Vue 组件的生命周期钩子，它们主要用于 keep-alive 组件。
+   * keep-alive 组件中生效，用于缓存组件而不销毁，适用于 Tab 切换、列表缓存等场景。
+   */
+  // 当组件被 激活（进入可见状态） 时触发
+  activated(){
+    console.log('Home activated...');
+    if(this.path !== this.$route.path) {
+      this.$router.push(this.path);
+    }
+  },
+  // 当组件被 停用（离开可见状态） 时触发。
+  deactivated(){
+    console.log('Home deactivated...');
+  }
 }
 </script>
 
