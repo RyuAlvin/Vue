01d9fa8d4969f4b58d31527485690ca07befe6ad
ryualvin <lsben1014@gmail.com>
2025-02-26 23:25:19 +0900

    TabBar图标替换png->svg

--- Vue/18-tabbar/src/App.vue
+++ Vue/18-tabbar/src/App.vue
@@ -3,23 +3,23 @@
     <router-view></router-view>
     <tab-bar>
       <tab-bar-item link="/home">
-        <img slot="item-img" src="./assets/img/tabbar/home.png" alt=""/>
-        <img slot="item-img-active" src="./assets/img/tabbar/home_active.png" alt=""/>
+        <img slot="item-img" src="./assets/img/tabbar/home.svg" alt=""/>
+        <img slot="item-img-active" src="./assets/img/tabbar/home_active.svg" alt=""/>
         <div slot="item-text">首页</div>
       </tab-bar-item>
       <tab-bar-item link="/category">
-        <img slot="item-img" src="./assets/img/tabbar/category.png" alt=""/>
-        <img slot="item-img-active" src="./assets/img/tabbar/category_active.png" alt=""/>
+        <img slot="item-img" src="./assets/img/tabbar/category.svg" alt=""/>
+        <img slot="item-img-active" src="./assets/img/tabbar/category_active.svg" alt=""/>
         <div slot="item-text">分类</div>
       </tab-bar-item>
       <tab-bar-item link="/cart">
-        <img slot="item-img" src="./assets/img/tabbar/cart.png" alt=""/>
-        <img slot="item-img-active" src="./assets/img/tabbar/cart_active.png" alt=""/>
+        <img slot="item-img" src="./assets/img/tabbar/shopcart.svg" alt=""/>
+        <img slot="item-img-active" src="./assets/img/tabbar/shopcart_active.svg" alt=""/>
         <div slot="item-text">购物车</div>
       </tab-bar-item>
       <tab-bar-item link="profile">
-        <img slot="item-img" src="./assets/img/tabbar/profile.png" alt=""/>
-        <img slot="item-img-active" src="./assets/img/tabbar/profile_active.png" alt=""/>
+        <img slot="item-img" src="./assets/img/tabbar/profile.svg" alt=""/>
+        <img slot="item-img-active" src="./assets/img/tabbar/profile_active.svg" alt=""/>
         <div slot="item-text">我的</div>
       </tab-bar-item>
     </tab-bar>
