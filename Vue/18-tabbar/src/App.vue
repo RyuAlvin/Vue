cc106df0773fefcd935ed87426a28e731520910f
ryualvin <lsben1014@gmail.com>
2025-02-26 19:58:15 +0900

    tabbar传入active图片

--- Vue/18-tabbar/src/App.vue
+++ Vue/18-tabbar/src/App.vue
@@ -3,18 +3,22 @@
     <tab-bar>
       <tab-bar-item>
         <img slot="item-img" src="./assets/img/tabbar/home.png" alt=""/>
+        <img slot="item-img-active" src="./assets/img/tabbar/home_active.png" alt=""/>
         <div slot="item-text">首页</div>
       </tab-bar-item>
       <tab-bar-item>
         <img slot="item-img" src="./assets/img/tabbar/category.png" alt=""/>
+        <img slot="item-img-active" src="./assets/img/tabbar/category_active.png" alt=""/>
         <div slot="item-text">分类</div>
       </tab-bar-item>
       <tab-bar-item>
         <img slot="item-img" src="./assets/img/tabbar/cart.png" alt=""/>
+        <img slot="item-img-active" src="./assets/img/tabbar/cart_active.png" alt=""/>
         <div slot="item-text">购物车</div>
       </tab-bar-item>
       <tab-bar-item>
         <img slot="item-img" src="./assets/img/tabbar/profile.png" alt=""/>
+        <img slot="item-img-active" src="./assets/img/tabbar/profile_active.png" alt=""/>
         <div slot="item-text">我的</div>
       </tab-bar-item>
     </tab-bar>
--- Vue/18-tabbar/src/components/tabbar/TabBarItem.vue
+++ Vue/18-tabbar/src/components/tabbar/TabBarItem.vue
@@ -1,13 +1,25 @@
 <template>
   <div class="tab-bar-item">
-    <slot name="item-img"></slot>
-    <slot name="item-text"></slot>
+    <div v-if="!isActive">
+      <slot name="item-img"></slot>
+    </div>
+    <div v-else>
+      <slot name="item-img-active"></slot>
+    </div>
+    <div :class="{active: isActive}">
+      <slot name="item-text"></slot>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'TarBarItem'
+  name: 'TarBarItem',
+  data() {
+    return {
+      isActive: false
+    }
+  },
 }
 </script>
 
@@ -25,5 +37,9 @@ export default {
 	margin-top: 3px;
 	vertical-align: middle;
   margin-bottom: 2px;
+}
+
+.active {
+  color: hotpink;
 }
 </style>
