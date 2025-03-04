a4a8170261340a6bcd2467009ad84544e6fafb72
ryualvin <lsben1014@gmail.com>
2025-03-04 19:15:00 +0900

    用props实现父子组件状态共享雏形

---
+++ Vue/20-learnvuex/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <button @click="counter++">按钮++</button>
+    <button @click="counter--">按钮--</button>
+    <h2>App ===> {{ counter }}</h2>
+    <hr/>
+    <hello-vuex :counter="counter"></hello-vuex>
+  </div>
+</template>
+
+<script>
+import HelloVuex from '@/components/HelloVuex'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      counter: 0
+    }
+  },
+  components: {
+    HelloVuex
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ Vue/20-learnvuex/src/components/HelloVuex.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="HelloVuex">
+    <h2>HelloVuex ===> {{ counter }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'HelloVuex',
+  props: {
+    counter: Number
+  }
+}
+</script>
+
+<style>
+
+</style>
