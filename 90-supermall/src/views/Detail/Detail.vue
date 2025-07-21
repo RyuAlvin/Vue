562e4347458fb544aa5c9cd7c1a84cd5575b2beb
ryualvin <lsben1014@gmail.com>
2025-07-21 16:24:50 +0900

    详情页数据请求以及轮播图展示

--- 90-supermall/src/views/Detail/Detail.vue
+++ 90-supermall/src/views/Detail/Detail.vue
@@ -1,20 +1,30 @@
 <template>
   <div>
     <detail-nav-bar/>
+    <detail-swiper :images="topImages"/>
   </div>
 </template>
 
 <script>
 import DetailNavBar from './childComps/DetailNavBar.vue';
+import DetailSwiper from './childComps/DetailSwiper.vue';
+import { getDetailData } from '@/network/detail';
 export default {
-  components: { DetailNavBar },
+  components: { DetailNavBar, DetailSwiper },
   data() {
     return {
-      id: null
+      id: null,
+      topImages: []
     }
   },
   created() {
+    // 保存主页传过来的详情页id
     this.id = this.$route.params.id;
+
+    // 获取详情页数据
+    getDetailData(this.id).then(res => {
+      this.topImages = res.result.itemInfo.topImages;
+    })
   },
 }
 </script>
--- 90-supermall/src/views/Detail/childComps/DetailNavBar.vue
+++ 90-supermall/src/views/Detail/childComps/DetailNavBar.vue
@@ -1,19 +1,5 @@
 <template>
   <nav-bar>
-    /**
-     * 1、详情页导航栏分为三个部分：
-     *    返回按钮，导航内容
-     * 2、详情页导航栏属于详情页的子组件
-     *    在详情页中建子文件夹，对导航栏共通组件包一层作为详情页专用的导航栏组件
-     * 3、导航内容部分（center插槽）
-     *    3.1、定义一个数组存放导航文字内容
-     *    3.2、外层display:flex布局，内层flex:1让其均等分间隔
-     *    3.3、添加点击事件，保存当前点击的currentIndex
-     *    3.4、currentIndex=index的时候，激活点击类
-     * 4、返回按钮（left插槽）
-     *    4.1、引用图片
-     *    4.2、添加点击事件，通过go(-1)或者back返回至迁移前页面
-     */
     <div slot="left" class="back-nav" @click="back">
       <img src="~assets/img/common/back.svg"/>
     </div>
@@ -30,6 +16,20 @@
 </template>
 
 <script>
+/**
+ * 1、详情页导航栏分为三个部分：
+ *    返回按钮，导航内容
+ * 2、详情页导航栏属于详情页的子组件
+ *    在详情页中建子文件夹，对导航栏共通组件包一层作为详情页专用的导航栏组件
+ * 3、导航内容部分（center插槽）
+ *    3.1、定义一个数组存放导航文字内容
+ *    3.2、外层display:flex布局，内层flex:1让其均等分间隔
+ *    3.3、添加点击事件，保存当前点击的currentIndex
+ *    3.4、currentIndex=index的时候，激活点击类
+ * 4、返回按钮（left插槽）
+ *    4.1、引用图片
+ *    4.2、添加点击事件，通过go(-1)或者back返回至迁移前页面
+ */
 import NavBar from '@/components/common/navBar/NavBar.vue';
 export default {
   data() {
