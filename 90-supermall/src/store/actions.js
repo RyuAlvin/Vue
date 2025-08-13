b98dde67623b01cfe87a5a7db8f06ed7fd09a1bd
ryualvin <lsben1014@gmail.com>
2025-08-13 21:44:08 +0900

    Vuex中代码的重构（创建mutation-types）

--- 90-supermall/src/store/actions.js
+++ 90-supermall/src/store/actions.js
@@ -1,12 +1,17 @@
+import {
+  ADD_COUNTER,
+  ADD_PRODUCT
+} from './mutation-types'
+
 export default {
   // context => { state, getters, commit, dispatch, rootState, rootGetters}
   addToCart({state, commit}, payload) {
     const product = state.cartList.find((item) => item.id === payload.id);
     if(product) {
-      commit('addCounter', product);
+      commit(ADD_COUNTER, product);
     } else {
       payload.count = 1;
-      commit('addProduct', payload);
+      commit(ADD_PRODUCT, payload);
     }
 
     console.log(state.cartList);
--- 90-supermall/src/store/mutations.js
+++ 90-supermall/src/store/mutations.js
@@ -1,10 +1,15 @@
+import {
+  ADD_COUNTER,
+  ADD_PRODUCT
+} from './mutation-types'
+
 export default {
   // 增加数量
-  addCounter(state, payload) {
+  [ADD_COUNTER](state, payload) {
     payload.count++;
   },
   // 添加商品
-  addProduct(state, payload) {
+  [ADD_PRODUCT](state, payload) {
     state.cartList.push(payload);
   }
 }
