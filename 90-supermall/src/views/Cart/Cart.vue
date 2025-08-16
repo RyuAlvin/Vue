2330093d45e3ce648ac9c8ba1a991a129c7c5aa4
ryualvin <lsben1014@gmail.com>
2025-08-16 22:18:03 +0900

    每次激活Cart组件时刷新滚动高度

--- 90-supermall/src/views/Cart/Cart.vue
+++ 90-supermall/src/views/Cart/Cart.vue
@@ -3,7 +3,7 @@
     <nav-bar>
       <div slot="center">购物车({{ length }})</div>
     </nav-bar>
-    <scroll class="content">
+    <scroll class="content" ref="scroll">
       <cart-list />
     </scroll>
   </div>
@@ -30,6 +30,14 @@ export default {
     ...mapGetters({
       length: 'cartLength'
     })
+  },
+  activated(){
+    /**
+     * Cart.vue是被<keep-alive>缓存的组件，当Cart组件被激活（即进入Cart组件）的时候，
+     * 当前的购物车件数撑开的高度和第一次加载Cart组件的可滚动高度可能存在不匹配的情况。
+     * 所以，需要在每次激活的时候取刷新可滚动高度，才能保证正常滚动。
+     */
+    this.$refs.scroll.refresh();
   }
 }
 </script>
