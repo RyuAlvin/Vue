9c9b7139dfef8d7793de8fbaa36389858db9355c
ryualvin <lsben1014@gmail.com>
2025-08-16 11:01:25 +0900

    购物车顶部导航栏完善

--- 90-supermall/src/views/Cart/Cart.vue
+++ 90-supermall/src/views/Cart/Cart.vue
@@ -1,13 +1,25 @@
 <template>
-  <h2>购物车</h2>
+  <div>
+    <nav-bar>
+      <div slot="center">购物车</div>
+    </nav-bar>
+  </div>
 </template>
 
 <script>
+import NavBar from '@/components/common/navBar/NavBar.vue';
+
 export default {
-  
+  name: 'Cart',
+  components: {
+    NavBar
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+.nav-bar {
+  background-color: var(--color-tint);
+  color: #fff;
+}
 </style>
